4a4a80ecbd2dd60854c2cb77b314763c5a03224f
Zineddine Zahouani <43182889+zineddinezino@users.noreply.github.com>
2022-10-17 22:56:49 +0200

    Unit Test PostController

--- src/test/java/com/example/RedditClone/Post/PostControllerTest.java
+++ src/test/java/com/example/RedditClone/Post/PostControllerTest.java
@@ -1,8 +1,11 @@
 package com.example.RedditClone.Post;
 
 import com.example.RedditClone.controller.PostController;
+import com.example.RedditClone.dto.post.PostRequestDto;
 import com.example.RedditClone.dto.post.PostResponseDto;
 import com.example.RedditClone.service.PostService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,22 +22,57 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 public class PostControllerTest {
 
     @InjectMocks
-    PostController postController;
-
+    private PostController postController;
     @Mock
-    PostService postService;
+    private PostService postService;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private PostRequestDto postRequestDto;
 
-    PostResponseDto postResponseDto;
-    MockMvc mockMvc;
+    private MockMvc mockMvc;
 
     @BeforeEach
-    void setUp() throws Exception  {
+    void setUp() throws Exception {
         MockitoAnnotations.openMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(postController).build();
 
-        postResponseDto = PostResponseDto.builder()
+
+    }
+
+    @Test
+    void should_return_post_when_get_post_by_id() throws Exception {
+
+        var postResponseDto = buildPostResponseDto();
+
+        Mockito.when(postService.getPost(1L)).thenReturn(postResponseDto);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/posts/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.postName", Matchers.is("Get Post Test")));
+    }
+
+    @Test
+    void should_return_created_status_when_create_new_post() throws Exception {
+        var postRequestDto = buildPostRequestDto();
+        Mockito.doNothing().when(postService).createPost(postRequestDto);
+
+        var content = toJson(postRequestDto);
+
+        var mockPostRequest = MockMvcRequestBuilders
+                .post("/api/posts/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(content);
+
+        mockMvc.perform(mockPostRequest)
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+
+    private PostResponseDto buildPostResponseDto() {
+        return PostResponseDto.builder()
                 .postId(1L)
-                .postName("Test")
+                .postName("Get Post Test")
                 .postDescription("Nothing")
                 .postUrl("www.post.com")
                 .postVotingCount(0)
@@ -45,13 +83,17 @@ public class PostControllerTest {
                 .build();
     }
 
-    @Test
-    void TestGetPost() throws Exception{
-        Mockito.when(postService.getPost(1L)).thenReturn(postResponseDto);
-        mockMvc.perform(MockMvcRequestBuilders
-                .get("/api/posts/1")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.postName", Matchers.is("Test")));
+    private PostRequestDto buildPostRequestDto() {
+        return postRequestDto = PostRequestDto.builder()
+                .postId(2L)
+                .postName("Create Post Test")
+                .postUrl("www.post.com")
+                .postDescription("Nothing")
+                .subredditName("Nothing")
+                .build();
+    }
+
+    private String toJson(PostRequestDto postRequestDto) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(postRequestDto);
     }
 }
